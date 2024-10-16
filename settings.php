d3717a27eded95cb9aeb367c0004623f76f8ce43
vjosset <lkjasd>
2024-10-16 06:47:10 -0400

    Tweak layout of Settings screen

--- settings.php
+++ settings.php
@@ -46,25 +46,25 @@
 		<!-- Settings -->
 		<div class="container">
 			<h2>Display</h2>
-			<div class="m-2 row">
+			<div class="m-2 row m-0 p-0">
 				
-				<div class="col-12 col-md-6">
-					<h6>Editions</h6>
+				<div class="col-12 col-md-6 m-0 p-0">
+					<h5>Editions</h5>
 					Select which editions to show.<br/>
 					<button class="btn h3" style="width: 120px;" ng-click="setSetting('edition', '');" ng-class="settings['edition'] == '' || settings['edition'] == null ? 'btn-primary': 'btn-secondary'">
-						<h6>Both</h6>
+						<h5>Both</h5>
 					</button>
 					<button class="btn h3" style="width: 120px;" ng-click="setSetting('edition', 'kt24');" ng-class="settings['edition'] == 'kt24' ? 'btn-primary': 'btn-secondary'">
-						<h6>KT24</h6>
+						<h5>KT24</h5>
 					</button>
 					<button class="btn h3" style="width: 120px;" ng-click="setSetting('edition', 'kt21');" ng-class="settings['edition'] == 'kt21' ? 'btn-primary': 'btn-secondary'">
-						<h6>KT21</h6>
+						<h5>KT21</h5>
 					</button>
 					<br/><br/>
 				</div>
 
-				<div class="col-12 col-md-6">
-					<h6>Portraits</h6>
+				<div class="col-12 col-md-6 m-0 p-0">
+					<h5>Portraits</h5>
 					Displays the portraits for operatives and rosters if enabled.<br/>
 					<button class="btn h3" style="width: 120px;" ng-click="setSetting('display', 'card');" ng-class="settings['display'] == 'card' || settings['display'] == null ? 'btn-primary': 'btn-secondary'">
 						<!-- i class="fas fa-check fa-fw" style="top: 2px; left: 2px;" ng-if="settings['display'] == 'card' || settings['display'] == null"></i -->
@@ -79,8 +79,8 @@
 					<br/><br/>
 				</div>
 				
-				<div class="col-12 col-md-6">
-					<h6>Operative Name/Type</h6>
+				<div class="col-12 col-md-6 m-0 p-0">
+					<h5>Operative Name/Type</h5>
 					Show Operative Name or Type first on cards.<br/>
 					<button class="btn h3" style="width: 120px;" ng-click="setSetting('opnamefirst', 'y');" ng-class="settings['opnamefirst'] == 'y' || settings['opnamefirst'] == null ? 'btn-primary': 'btn-secondary'">
 						<!-- i class="fas fa-check fa-fw" style="top: 2px; left: 2px;" ng-if="settings['display'] == 'card' || settings['display'] == null"></i -->
@@ -95,8 +95,8 @@
 					<br/><br/>
 				</div>
 				
-				<div class="col-12 col-md-6">
-					<h6>Auto-Generate Operative Names</h6>
+				<div class="col-12 col-md-6 m-0 p-0">
+					<h5>Auto-Generate Operative Names</h5>
 					Auto-generates operative names if enabled. If disabled, uses the operative type as its name.<br/>
 					<button class="btn h3" style="width: 120px;" ng-click="setSetting('useoptypeasname', 'n');" ng-class="settings['useoptypeasname'] == 'n' || settings['useoptypeasname'] == null ? 'btn-primary': 'btn-secondary'">
 						<!-- i class="fas fa-check fa-fw" style="top: 2px; left: 2px;" ng-if="settings['display'] == 'card' || settings['display'] == null"></i -->
@@ -111,8 +111,8 @@
 					<br/><br/>
 				</div>
 				
-				<div class="col-12 col-md-6">
-					<h6>Operative Numbers</h6>
+				<div class="col-12 col-md-6 m-0 p-0">
+					<h5>Operative Numbers</h5>
 					Displays operative numbers in the roster and dashboard if enabled.<br/>
 					<button class="btn h3" style="width: 120px;" ng-click="setSetting('showopseq', 'y');" ng-class="settings['showopseq'] == 'y' ? 'btn-primary': 'btn-secondary'">
 						<!-- i class="fas fa-check fa-fw" style="top: 2px; left: 2px;" ng-if="settings['showopseq'] == 'y'"></i -->
@@ -127,8 +127,8 @@
 					<br/><br/>
 				</div>
 				
-				<div class="col-12 col-md-6">
-					<h6>Operative IDs</h6>
+				<div class="col-12 col-md-6 m-0 p-0">
+					<h5>Operative IDs</h5>
 					Displays operative IDs (e.g. "[HGNR]" for Heavy Gunner) in the roster and dashboard if enabled.<br/>
 					<button class="btn h3" style="width: 120px;" ng-click="setSetting('showopid', 'y');" ng-class="settings['showopid'] == 'y' ? 'btn-primary': 'btn-secondary'">
 						<!-- i class="fas fa-check fa-fw" style="top: 2px; left: 2px;" ng-if="settings['showopseq'] == 'y'"></i -->
@@ -143,38 +143,12 @@
 					<br/><br/>
 				</div>
 				
-				<div class="col-12 col-md-6">
-					<h6>Narrative Info</h6>
-					Shows or hides narrative play information (Battle Honours, Rare Equipment, XP)
-					<br/>
-					<button class="btn h3" style="width: 120px;" ng-click="setSetting('shownarrative', 'y');" ng-class="settings['shownarrative'] == 'y' ? 'btn-primary': 'btn-secondary'">
-						<i class="pointer fas fa-check fa-fw"></i><br/>
-						Show
-					</button>
-					<button class="btn h3" style="width: 120px;" ng-click="setSetting('shownarrative', 'n');" ng-class="settings['shownarrative'] == 'n' ? 'btn-primary': 'btn-secondary'">
-						<i class="pointer fas fa-times fa-fw"></i><br/>
-						Hide
-					</button>
-					<br/><br/>
-				</div>
-				
-				<div class="col-12 col-md-6">
-					<h6>Close Quarters</h6>
-					Automatically adds Lethal 5+ to Weapons with the Blast x, Splash x and/or Torrent x rules.
-					<br/>
-					<button class="btn h3" style="width: 120px;" ng-click="setSetting('closequarters', 'y');" ng-class="settings['closequarters'] == 'y' ? 'btn-primary': 'btn-secondary'">
-						<i class="pointer fas fa-check fa-fw"></i><br/>
-						Enable
-					</button>
-					<button class="btn h3" style="width: 120px;" ng-click="setSetting('closequarters', 'n');" ng-class="settings['closequarters'] == 'n' ? 'btn-primary': 'btn-secondary'">
-						<i class="pointer fas fa-times fa-fw"></i><br/>
-						Disable
-					</button>
-					<br/><br/>
-				</div>
-				
-				<div class="col-12 col-md-6">
-					<h6>Auto-Apply Equipment Modifiers</h6>
+			</div>
+
+			<h2 class="line-top-light">Game Options</h2>
+			<div class="m-2 row">
+				<div class="col-12 col-md-6 m-0 p-0">
+					<h5>Auto-Apply Equipment Modifiers</h5>
 					Automatically applies equipment modifiers to operatives and weapons if enabled.
 					<br/>
 					<button class="btn h3" style="width: 120px;" ng-click="setSetting('applyeqmods', 'y');" ng-class="settings['applyeqmods'] == 'y' ? 'btn-primary': 'btn-secondary'">
@@ -190,8 +164,8 @@
 					<br/><br/>
 				</div>
 				
-				<div ng-if="settings['applyeqmods'] == 'y'" class="col-12 col-md-6">
-					<h6>Hide Auto-Applied Equipments</h6>
+				<div class="col-12 col-md-6 m-0 p-0" ng-if="settings['applyeqmods'] == 'y'">
+					<h5>Hide Auto-Applied Equipments</h5>
 					Hides equipments from the "Equipment" list if they were auto-applied to Abilities, Actions, or Operative/Weapon stats
 					<br/>
 					<button class="btn h3" style="width: 120px;" ng-click="setSetting('hideappliedeqmods', 'y');" ng-class="settings['hideappliedeqmods'] == 'y' ? 'btn-primary': 'btn-secondary'">
@@ -206,13 +180,43 @@
 					</button>
 					<br/><br/>
 				</div>
+
+				<div class="col-12 col-md-6 m-0 p-0">
+					<h5>Narrative Info</h5>
+					Shows or hides narrative play information (Battle Honours, Rare Equipment, XP)
+					<br/>
+					<button class="btn h3" style="width: 120px;" ng-click="setSetting('shownarrative', 'y');" ng-class="settings['shownarrative'] == 'y' ? 'btn-primary': 'btn-secondary'">
+						<i class="pointer fas fa-check fa-fw"></i><br/>
+						Show
+					</button>
+					<button class="btn h3" style="width: 120px;" ng-click="setSetting('shownarrative', 'n');" ng-class="settings['shownarrative'] == 'n' ? 'btn-primary': 'btn-secondary'">
+						<i class="pointer fas fa-times fa-fw"></i><br/>
+						Hide
+					</button>
+					<br/><br/>
+				</div>
+				
+				<div class="col-12 col-md-6 m-0 p-0">
+					<h5>Close Quarters</h5>
+					Automatically adds Lethal 5+ to Weapons with the Blast x, Splash x and/or Torrent x rules.
+					<br/>
+					<button class="btn h3" style="width: 120px;" ng-click="setSetting('closequarters', 'y');" ng-class="settings['closequarters'] == 'y' ? 'btn-primary': 'btn-secondary'">
+						<i class="pointer fas fa-check fa-fw"></i><br/>
+						Enable
+					</button>
+					<button class="btn h3" style="width: 120px;" ng-click="setSetting('closequarters', 'n');" ng-class="settings['closequarters'] == 'n' ? 'btn-primary': 'btn-secondary'">
+						<i class="pointer fas fa-times fa-fw"></i><br/>
+						Disable
+					</button>
+					<br/><br/>
+				</div>
 			</div>
 			
 			<h2 class="line-top-light">Dashboard Defaults</h2>
 			<div class="m-2 row">
-				<div class="col-12 col-md-6">
+				<div class="col-12 col-md-6 m-0 p-0">
 					<div class="row">
-						<h4 class="d-inline col-6">Starting VP</h4>
+						<h4 class="d-inline col-6">Starting VP</h5>
 						<select class="col-2" name="startvp" ng-model="settings['startvp']" ng-change="setSetting('startvp', settings['startvp']);">
 							<option value="0">0</option>
 							<option value="1">1</option>
@@ -223,9 +227,9 @@
 					<br/><br/>
 				</div>
 				
-				<div class="col-12 col-md-6">
+				<div class="col-12 col-md-6 m-0 p-0">
 					<div class="row">
-						<h4 class="d-inline col-6">Starting CP</h4>
+						<h4 class="d-inline col-6">Starting CP</h5>
 						<select class="col-2" name="startcp" ng-model="settings['startcp']" ng-change="setSetting('startcp', settings['startcp']);">
 							<option value="0">0</option>
 							<option value="1">1</option>
@@ -238,8 +242,8 @@
 					<br/><br/>
 				</div>
 				
-				<div class="col-12 col-md-6">
-					<h4>Auto-Increment CP</h4>
+				<div class="col-12 col-md-6 m-0 p-0">
+					<h5>Auto-Increment CP</h5>
 					Automatically increases Command Points when moving to the next Turning Point if enabled.
 					<br/>
 					<button class="btn h3" style="width: 120px;" ng-click="setSetting('autoinccp', 'y');" ng-class="settings['autoinccp'] == 'y' ? 'btn-primary': 'btn-secondary'">
@@ -253,8 +257,8 @@
 					<br/><br/>
 				</div>
 				
-				<div class="col-12 col-md-6">
-					<h4>Default Operative Order</h4>
+				<div class="col-12 col-md-6 m-0 p-0">
+					<h5>Default Operative Order</h5>
 					The default order to give your operatives when resetting the dashboard.
 					<br/>
 					<button class="btn h3" style="width: 120px;" ng-click="setSetting('defaultoporder', 'engage');" ng-class="settings['defaultoporder'] == 'engage' ? 'btn-primary': 'btn-secondary'">
